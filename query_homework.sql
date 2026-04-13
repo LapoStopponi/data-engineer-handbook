6482132f87d912978e3df5b8b2c5b3d874a9db69
Lapo Stopponi <166521228+LapoStopponi@users.noreply.github.com>
2026-04-13 08:56:38 +0000

    Implementata logica di inserimento cumulativo 1997-1998

--- query_homework.sql
+++ query_homework.sql
@@ -18,6 +18,8 @@ CREATE TYPE season_stats AS (
 the players so that we don't duplicate them, and then we'll have the array of
 seasons stats */
 
+CREATE TYPE scoring_class AS ENUM ('star', 'good', 'average', 'bad');
+
 CREATE TABLE players(
     player_name TEXT,
     height TEXT,
@@ -27,10 +29,76 @@ CREATE TABLE players(
     draft_round TEXT,
     draft_number TEXT,
     season_stats season_stats[],
+    scoring_class scoring_class,
+    years_since_last_season INTEGER,
     current_season INTEGER,
     PRIMARY KEY(player_name, current_season)
 )
 
 -- Those are the values that don't really change in the dataset
 
-select min(season) from player_seasons;
+-- this is gonna give us the cumulation between today and yesterday
+INSERT INTO players
+WITH yesterday as (
+    SELECT * from players
+    WHERE current_season = 2000
+),
+    today AS(
+        SELECT * FROM player_seasons
+        WHERE season = 2001
+    )
+
+
+-- you want to coalesce the values that are not temporal
+SELECT 
+    COALESCE(t.player_name, y.player_name) as player_name, 
+    COALESCE(t.height, y.height) as height, 
+    COALESCE(t.college, y.college) as college, 
+    COALESCE(t.country, y.country) as country, 
+    COALESCE(t.draft_year, y.draft_year) as draft_year, 
+    COALESCE(t.draft_round, y.draft_round) as draft_round, 
+    COALESCE(t.draft_number, y.draft_number) as draft_number, 
+    CASE WHEN y.season_stats IS NULL
+        THEN ARRAY [ROW(
+            t.season,
+            t.gp,
+            t.pts,
+            t.reb,
+            t.ast
+            )::season_stats]
+    WHEN t.season IS NOT NULL THEN y.season_stats || ARRAY [ROW(
+            t.season,
+            t.gp,
+            t.pts,
+            t.reb,
+            t.ast
+            )::season_stats]
+    ELSE y.season_stats
+    END season_stats,
+    COALESCE(t.season, y.current_season + 1) as current_season
+     FROM today t FULL OUTER JOIN yesterday y 
+    ON t.player_name = y.player_name;
+
+SELECT * FROM players
+where current_season = 2001 and player_name = 'Michael Jordan';
+
+/* U can also unnested it if you need it, it is very useful to use cumulative
+tables: 
+
+with unnested as(
+    SELECT player_name,
+        UNNEST(season_stats)::season_stats ad season_stats
+    FROM players
+    WHERE current_season = 2001
+)
+
+SELECT player_name,
+    (season_stats::season::stats).*
+FROM unnested;
+*/
+
+
+SELECT * FROM players
+WHERE current_season = 2001;
+
+DROP TABLE players;
